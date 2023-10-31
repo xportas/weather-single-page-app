15f3d9fe4d896ddde27447ad8b66bebf13e5ff58
Xabier Portas <105586880+xportas@users.noreply.github.com>
2023-10-31 20:18:30 +0100

    feat(add): add functions to make an api call

--- assets/js/inputWeather.js
+++ assets/js/inputWeather.js
@@ -1,49 +1,126 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=";
+    var apiCurrentByNameUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+    var apiCurrentByLatLonUrl = "https://api.openweathermap.org/data/2.5/weather?";
+    var api5DaysByNameUrl = "api.openweathermap.org/data/2.5/forecast?q=";
+    var api5DaysByLatLonUrl = "api.openweathermap.org/data/2.5/forecast?";
     var apiKey = "0ad2ed4b77ee83cd4eddf03827379892";
 
     var input = $('#location-input')
-    var btn = $('#btn-search')
+    var btnSearch = $('#btn-search')
+    var aCurrent = $('#a-current-city')
 
-    function searchCity(city){
-        $.ajax({
+    /* POSIBLE NECESIDAD PARA VALIDAR
+    var currentDate = new Date();
+    var currentDay = currentDate.getDate();
+    var currentMonth = currentDate.getMonth() + 1;
+    var currentYear = currentDate.getFullYear();
+    var formatedDate = currentYear + "-" + currentMonth + "-" + currentDay;*/
+
+    function searchCityWeatherByName(city) {
+        $.ajax({ // current weather call
             type: "GET",
-            url: apiUrl + city + "&appid=" + apiKey, 
+            url: apiCurrentByNameUrl + city + "&appid=" + apiKey,
             dataType: "json",
-            success: function(response){
-                $('table > tbody').empty() //TODO gestionar cómo se muestra por pantalla la información
+            success: function (response) {
+                $('#city-weather-content').empty();
 
                 //vemos la respuesta de la api por la consola para depurar
-                //console.log(response)
+                console.log(response)
 
-                for (let i=0; i<response.length; i++){
-                    $('table tbody').append('<tr><th scope="row">' + response[i].name + '</th><td>' + response[i].lat + '</td><td>' + response[i].lon + '</td><td>' + response[i].country + '</td></tr>');
-                    //TODO lo mismo, esto construye la info en el html
-                }
+                let name = response.name;
+                let icon = response.weather.icon;
+                let tempMin = response.main.temp_min;
+                let tempMax = response.main.temp_max;
 
-            }, 
-            error: function(error){
+                $('#city-weather-content').append('<div class="row mt-3"><h2 class="d-flex justify-content-center">' + name + '</h2></div><div class="row col-9"><img src="assets/images/' + icon + '.png" alt="current-weather-img"></div><div class="row"><p class="d-flex justify-content-center">Min: ' + tempMin + 'º  -  Max: ' + tempMax + 'º</p></div>');
+            },
+            error: function (error) {
                 console.error("Error: ", error);
             },
         })
 
-    } 
 
-    btn.on('click', function(event){
+        $.ajax({ // forecasted weather call
+            type: "GET",
+            url: api5DaysByNameUrl + city + "&appid=" + apiKey,
+            dataType: "json",
+            success: function (response) {
+                //vemos la respuesta de la api por la consola para depurar
+                console.log(response);
+
+
+                for (let i = 0; i < response.length; i++) {
+                    
+                    //TODO VALIDACIONES
+                    
+                    if (response[i].name == city) {
+                        $('#city-weather-content').append('<div class="row"><div class="col-9"><p>' + TOCHANGE + '</p></div><div class="col-3"><p>' + TOCHANGE + '</p></div></div>');
+                    }
+                }
+            },
+            error: function (error) {
+                console.error("Error: ", error);
+            },
+        })
+    }
+
+    function searchCityWeatherByCurrentLocation(position) {
+        let lat = position.coord.lat;
+        let lon = position.coord.lon;
+        $.ajax({ // current weather call
+            type: "GET",
+            url: apiCurrentByLatLonUrl + 'lat=' + lat + '&lon=' + lon + "&appid=" + apiKey,
+            dataType: "json",
+            success: function (response) {
+                $('#city-weather-content').empty();
+
+                //vemos la respuesta de la api por la consola para depurar
+                console.log(response)
+
+                let name = response.name;
+                let icon = response.weather.icon;
+                let tempMin = response.main.temp_min;
+                let tempMax = response.main.temp_max;
+
+                $('#city-weather-content').append('<div class="row mt-3"><h2 class="d-flex justify-content-center">' + name + '</h2></div><div class="row col-9"><img src="assets/images/' + icon + '.png" alt="current-weather-img"></div><div class="row"><p class="d-flex justify-content-center">Min: ' + tempMin + 'º  -  Max: ' + tempMax + 'º</p></div>');
+            },
+            error: function (error) {
+                console.error("Error: ", error);
+            },
+        })
+
+
+        // TODO : HACER CALL PARA LA PREVISION DE 5 DIAS
+        
+
+    }
+
+    function errorGetCurrentLocation() {
+            alert("Unable to retrieve your location for weather");
+        }
+
+
+
+    //function to get user current location
+    function getCurrentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(searchCityWeatherByCurrentLocation, errorGetCurrentLocation);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
+    
+
+    // EVENT LISTENERS
+    btnSearch.on('click', function (event) {
         event.preventDefault();
-        searchCity(input.val())
+        searchCityWeatherByName(input.val());
     });
 
-    //CURRENT LOCATION: ver cómo funciona navigator.geolocation
-    //genera una función que coja la localización actual del usuario
-    // function getCurrentLocation(){
-    //     if (navigator.geolocation){
-    //         navigator.geolocation.getCurrentPosition(showPosition);
-    //     } else {
-    //         alert("Geolocation is not supported by this browser.");
-    //     }
-    // }
-    
+    aCurrent.on('click', function (event) {
+        event.preventDefault();
+        getCurrentLocation();
+    });
 
 });
